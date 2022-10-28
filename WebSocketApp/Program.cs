541c8ebef87dc00651138356a1272eb70cfdb7d6
Mahmoud Saad <saadallah2891996@gmail.com>
2022-10-28 07:55:52 +0400

    init

--- WebSocketApp/Program.cs
+++ WebSocketApp/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Topshelf;
+
 namespace WebSocketApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var existCode = HostFactory.New(x =>
+            {
+
+                x.SetDisplayName("TestService");
+
+                x.SetServiceName("TestService");
+
+                x.SetInstanceName("TestService");
+
+                x.SetDescription("TestService");
+
+
+                x.Service<TestService>(sc =>
+                {
+                    sc.ConstructUsing(s => new TestService());
+
+                    sc.WhenStarted(s => s.Start());
+
+                    sc.WhenStopped(s => s.Stop());
+
+                });
+
+
+
+                x.RunAsLocalSystem();
+
+            });
+
+
+            existCode.Run();
         }
     }
 }
---
+++ WebSocketApp/TestService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSocketApp
+{
+    public class TestService
+    {
+        private Timer _timer;
+        WebSocketServer wssv;
+
+        public TestService()
+        {
+            _timer = new Timer();
+
+            _timer.Elapsed += _timer_Elapsed;
+
+            wssv = new WebSocketServer("ws://localhost:8090");
+
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            wssv.AddWebSocketService<Payment>("/Payment");
+            wssv.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+            wssv.Stop();
+        }
+
+
+        public class Payment : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+
+                var MessageData = e.Data;
+
+
+                // Do Anything here
+
+
+
+                string result = "result :- " + MessageData;
+
+                // for specific user
+                Sessions[ID].Context.WebSocket.Send(result);
+
+                // For All Users
+
+                Sessions.Broadcast(result);
+
+            }
+        }
+    }
+}
